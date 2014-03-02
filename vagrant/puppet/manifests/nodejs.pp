7f2ef9332fc3a405074d85a1771a4c806b280b47
Ricky Dunlop <hello@rickydunlop.co.uk>
2014-03-02 23:27:40 +0000

    Install any local nodes packages
    The path to package.json can be configured in config.yaml via
    nodejs/package_path, if the file is found then the packages will be
    installed when the virtual machine is provisioned

--- vagrant/puppet/manifests/nodejs.pp
+++ vagrant/puppet/manifests/nodejs.pp
@@ -7,10 +7,18 @@ class { 'nodejs':
   manage_repo => true
 }
 
+# install global packages
 if count($nodejs_values['packages']) > 0 {
   package { $nodejs_values['packages']:
       ensure   => present,
       provider => 'npm',
       require  => Class['nodejs']
+  }
+}
+
+# Install local packages
+if defined(File[$nodejs_values['package_path']]) {
+  nodejs::npm { $nodejs_values['package_path']:
+    ensure  => present,
   }
 }
